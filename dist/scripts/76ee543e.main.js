7e4e64e51939a9be4e734c8d0192cad0462a8643
Kevin Bost <kevbost@gmail.com>
2014-07-28 12:27:23 -0400

    ghpages bug

--- dist/scripts/76ee543e.main.js
+++ dist/scripts/76ee543e.main.js
@@ -1,3 +1,15 @@
+var _;
+var logoIcon = _.template($(".logoIconTemplate").text());
+var	containerVar = _.template($(".containerTemplate").text());
+var	repos = _.template($(".mainTemplate").text());
+$.getJSON("https://api.github.com/users/kevbost/repos").done(function(a) {
+	renderRepos(a)
+}), $.getJSON("https://api.github.com/users/kevbost").done(function(a) {
+	renderUser(a)
+}), $.getJSON("https://api.github.com/users/kevbost").done(function(a) {
+	renderNav(a)
+}), $(".headContainer").append(logoIcon), $(".sidebar").append(containerVar);
+
 function renderRepos(a) {
 	a.forEach(function(a) {
 		var b = repos(a);
@@ -14,13 +26,3 @@ function renderUser(a) {
 	var b = containerVar(a);
 	$(".sidebar").prepend(b)
 }
-var logoIcon = _.template($(".logoIconTemplate").text());
-var	containerVar = _.template($(".containerTemplate").text());
-var	repos = _.template($(".mainTemplate").text());
-$.getJSON("https://api.github.com/users/kevbost/repos").done(function(a) {
-	renderRepos(a)
-}), $.getJSON("https://api.github.com/users/kevbost").done(function(a) {
-	renderUser(a)
-}), $.getJSON("https://api.github.com/users/kevbost").done(function(a) {
-	renderNav(a)
-}), $(".headContainer").append(logoIcon), $(".sidebar").append(containerVar);
